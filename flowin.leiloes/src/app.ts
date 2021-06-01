351fff1961cd548cce994c03fae9b245c1976c1f
Kethreen Lanes <77758983+laneskell@users.noreply.github.com>
2021-05-31 22:38:54 -0300

    change port

--- flowin.leiloes/src/app.ts
+++ flowin.leiloes/src/app.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors())
 
-const server = app.listen(process.env.PORT || 3003, () => {
+const server = app.listen(process.env.PORT || 3001, () => {
    if (server) {
       const address = server.address() as AddressInfo;
       console.log(`Server is running in http://localhost:${address.port}`);
